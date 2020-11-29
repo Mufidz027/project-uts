8bd5579fe34cd556d424834a6a6b6698b06a0949
Mufidz027 <almusyafir240@gmail.com>
2020-11-30 04:12:51 +0700

    Initial commit

---
+++ system/resources/views/Client/Template/header.blade.php
@@ -0,0 +1,13 @@
+<header class="masthead" style="background-image: url('{{url('public')}}/client/img/barber.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>BARBERSHOP</h1>
+            <span class="subheading"> Sebuah BLOG untuk mengubah tampilan anda menjadi tamvan </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
